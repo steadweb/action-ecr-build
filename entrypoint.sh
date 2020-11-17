5d3b1f8257ed00d6b9bd2bb04bde2585a42c2434
Luke Steadman <ljsteadman@gmail.com>
2020-11-17 22:08:07 +0000

    Set default region

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,9 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
+echo "[default]
+region=${AWS_ACCESS_KEY_ID}" > ~/.aws/config
+
 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com
 docker build -f ./Dockerfile -t ${ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com/${REPO}:${TAG} .
 docker push ${ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com/${REPO}:${TAG}
