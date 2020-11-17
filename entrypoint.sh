8364817d27ee7d3c957739287c103cb496aee931
Luke Steadman <ljsteadman@gmail.com>
2020-11-17 22:01:28 +0000

    Updated ecr and docker login

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,6 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-aws ecr get-login --no-include-email --region ${REGION}
+aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com
 docker build -f ./Dockerfile -t ${ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com/${REPO}:${TAG} .
 docker push ${ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com/${REPO}:${TAG}
